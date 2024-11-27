e568e4499b355720bdb6135b69439d0430e380fa
SlobodinLarch <dslobodin@larch-networks.com>
2024-11-27 09:39:18 +0200

    New Library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,21 +36,21 @@ android {
 }
 
 dependencies {
-    implementation ("androidx.core:core-ktx:1.15.0")
-    implementation ("androidx.appcompat:appcompat:1.6.1")
-    implementation ("com.google.android.material:material:1.9.0")
-    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("com.google.android.gms:play-services-maps:18.0.2")
+    implementation (libs.androidx.core.ktx)
+    implementation (libs.androidx.appcompat.v161)
+    implementation (libs.material.v190)
+    implementation (libs.androidx.constraintlayout.v214)
+    implementation (libs.play.services.maps.v1802)
 
 
-    implementation ("androidx.camera:camera-lifecycle:1.3.0")
-    implementation ("androidx.camera:camera-view:1.3.0")
-    implementation ("androidx.camera:camera-camera2:1.3.0")
-    implementation ("androidx.camera:camera-core:1.3.0")
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation (libs.androidx.camera.lifecycle.v130)
+    implementation (libs.androidx.camera.view.v130)
+    implementation (libs.androidx.camera.camera2.v130)
+    implementation (libs.androidx.camera.core.v130)
+    implementation(libs.okhttp)
 
-    testImplementation ("junit:junit:4.13.2")
-    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation (libs.junit)
+    androidTestImplementation (libs.androidx.junit.v115)
+    androidTestImplementation (libs.androidx.espresso.core.v351)
 
 }
